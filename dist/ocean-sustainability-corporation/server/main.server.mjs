9fbd93d9a5732cb0be8e7e6fe5354d99d06477b1
Muhammad Shayan <141303510+DevShayan404@users.noreply.github.com>
2024-05-20 22:36:57 +0500

    commit

--- dist/ocean-sustainability-corporation/server/main.server.mjs
+++ dist/ocean-sustainability-corporation/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-TFGE337C.mjs";import"./chunk-6PAZW22K.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-2QZ7JCGT.mjs";import"./chunk-6PAZW22K.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
