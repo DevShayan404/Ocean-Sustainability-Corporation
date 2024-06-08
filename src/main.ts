602bbe28fd2fdc33a05de7ed2cc93249adbbbf34
Muhammad Shayan <141303510+DevShayan404@users.noreply.github.com>
2024-06-08 20:48:14 +0500

    commit

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,12 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.error(err))
+  .then(() => {
+    const body = document.querySelector('body');
+    if (body) {
+      body.classList.add('app-loaded');
+    }
+  });
